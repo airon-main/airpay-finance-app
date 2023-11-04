8e5b4c8293955d7eea2853517f4693d837455218
airon-main <airon.pribadi@gmail.com>
2023-11-04 07:00:28 +0700

    fix:obscure_text_text_field

--- lib/widgets/custom.dart
+++ lib/widgets/custom.dart
@@ -16,39 +16,40 @@ class myTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    RxBool isChecked = false.obs;
-    return TextField(
-      autofocus: false,
-      controller: TextEditingController(),
-      style: TextStyle(color: darkcolor['contrast']),
-      decoration: InputDecoration(
-        hintText: hintText,
-        fillColor: darkcolor["carddark"],
-        filled: true,
-        hintStyle: TextStyle(
-            color: darkcolor['disabled'], fontWeight: FontWeight.normal),
-        suffixIcon: isObscured
-            ? IconButton(
-                icon: Obx(() {
-                  return isChecked.isFalse
-                      ? Icon(Icons.visibility, color: darkcolor['disabled'])
-                      : Icon(Icons.visibility_off,
-                          color: darkcolor['disabled']);
-                }),
-                onPressed: () {
-                  isChecked.toggle();
-                },
-              )
-            : null,
-        contentPadding: const EdgeInsets.all(10),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(5),
-          borderSide: BorderSide.none,
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: darkcolor['main'], width: 0.5),
-        ),
-      ),
-    );
+    RxBool isChecked = true.obs;
+    return Obx(() => TextField(
+          autofocus: false,
+          controller: TextEditingController(),
+          style: TextStyle(color: darkcolor['contrast']),
+          obscureText: isObscured ? isChecked.value : isObscured,
+          decoration: InputDecoration(
+            hintText: hintText,
+            fillColor: darkcolor["carddark"],
+            filled: true,
+            hintStyle: TextStyle(
+                color: darkcolor['disabled'], fontWeight: FontWeight.normal),
+            suffixIcon: isObscured
+                ? IconButton(
+                    icon: Obx(() {
+                      return isChecked.isTrue
+                          ? Icon(Icons.visibility, color: darkcolor['disabled'])
+                          : Icon(Icons.visibility_off,
+                              color: darkcolor['disabled']);
+                    }),
+                    onPressed: () {
+                      isChecked.toggle();
+                    },
+                  )
+                : null,
+            contentPadding: const EdgeInsets.all(10),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+              borderSide: BorderSide.none,
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: darkcolor['main'], width: 0.5),
+            ),
+          ),
+        ));
   }
 }
