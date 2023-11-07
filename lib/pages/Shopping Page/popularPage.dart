f4ced0072147ff13cc952d9f7820565e95287ad7
altantheprodigy <altanpoke9@sar.ac.id>
2023-11-07 13:29:24 +0700

    Style: [AP] fitur Re-Desain Shopping Page

--- lib/pages/Shopping Page/popularPage.dart
+++ lib/pages/Shopping Page/popularPage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'komponen/Component.dart';
 
-
 class popularPage extends StatefulWidget {
   const popularPage({Key? key}) : super(key: key);
 
@@ -14,20 +13,63 @@ class _popularPageState extends State<popularPage> {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-          color: AppColors.cardDark
-      ),
+      decoration: BoxDecoration(color: AppColors.cardDark),
       child: Column(
         children: [
           judulPage(title: "Most Popular", title1: "View more"),
-          shopCon(imagePath: 'assets/images/gamepad.png', title: "Top Up", imageAssetPath: "assets/images/TopUp.png"),
-          shopCon(imagePath: 'assets/images/listrik.png', title: "PLN", imageAssetPath: "assets/images/pln.png"),
-          shopCon(imagePath: 'assets/images/sehat.png', title: "BPJS", imageAssetPath: "assets/images/bpjs.png"),
-          shopCon(imagePath: 'assets/images/air.png', title: "PDAM", imageAssetPath: "assets/images/pam.png"),
-          SizedBox(height: 20,),
+          Row(
+            children: [
+              Expanded(
+                  child: shopCon(
+                      imagePath: 'assets/images/gamepad.png',
+                      title: "Top Up",
+                      imageAssetPath: "assets/images/topup.png")),
+              Expanded(
+                child: shopCon(
+                    imagePath: 'assets/images/listrik.png',
+                    title: "PLN",
+                    imageAssetPath: "assets/images/kelistrikan.png"),
+              )
+            ],
+          ),
+
+          Row(
+            children: [
+              Expanded(
+                  child: shopCon(
+                      imagePath: 'assets/images/sehat.png',
+                      title: "BPJS",
+                      imageAssetPath: "assets/images/kesehatan.png")),
+              Expanded(
+                child: shopCon(
+                    imagePath: 'assets/images/air.png',
+                    title: "PDAM",
+                    imageAssetPath: "assets/images/perairan.png"),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
           judulPage(title: "Promo", title1: "View more"),
-          promoPage(imageAssetPath: "assets/images/Rectangle 55.png", text1: "Mobbile Legends", text2: "999,999 Diamonds", text3: "Rp.10.000", text4: "99%"),
-          promoPage(imageAssetPath: "assets/images/gensin.png", text1: "Genshin Impact", text2: "999,999 Diamonds", text3: "Rp.10.000", text4: "99%")
+          promoPage(
+              imageAssetPath: "assets/images/Rectangle 55.png",
+              text1: "Mobbile Legends",
+              text2: "999,999 Diamonds",
+              text3: "Rp.10.000",
+              text4: "99%"),
+          promoPage(
+              imageAssetPath: "assets/images/gensin.png",
+              text1: "Genshin Impact",
+              text2: "999,999 Diamonds",
+              text3: "Rp.10.000",
+              text4: "99%"),
+          promoPage(
+              imageAssetPath: "assets/images/kuota.png",
+              text1: "Kuota",
+              text2: "999,999 Diamonds",
+              text3: "Rp.10.000",
+              text4: "99%"),
         ],
       ),
     );
