ded619c7b952f3f90d4d0c7fd90aba2d6d948aea
altantheprodigy <altanpoke9@sar.ac.id>
2023-11-08 12:39:38 +0700

    Style: [AP] Style Shop  Page

---
+++ lib/pages/cardPage/cardPage.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+import '../Shopping Page/komponen/Component.dart';
+import 'cardComponPage.dart';
+class myCardPage extends StatefulWidget {
+  const myCardPage({Key? key}) : super(key: key);
+
+  @override
+  State<myCardPage> createState() => _myCardPageState();
+}
+
+class _myCardPageState extends State<myCardPage> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final padSize = 16.0;
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(size.width * (padSize * 5 / size.width)),
+        child: Container(
+          color: AppColors.cardDark,
+          padding: EdgeInsets.all(size.width * (padSize / size.width)),
+          child: SafeArea(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "My Cards",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w900,
+                    fontFamily: 'Lato',
+                  ),
+                ),
+                SizedBox(
+                  width: size.width * (padSize / size.width),
+                ),
+                RoundedIconButton(
+                  icon: Icons.notifications_none,
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(color: AppColors.cardDark),
+              child: Column(
+                children: [
+
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
